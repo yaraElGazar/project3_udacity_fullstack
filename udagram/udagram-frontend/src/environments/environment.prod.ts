da850e28b8adaaeadf464469447f51362f809062
yaraElGazar <yara_elgazar@outlook.com>
2022-07-05 01:19:40 +0200

    code updates

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  apiHost: "udagram-eb-aws-dev.eba-wmjhp2tr.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
